13d799a7d157312238a13b7eb2fab510ec62e000
Mr.True.China <mr.true.china@gmail.com>
2018-10-14 11:56:00 +0900

    Initial commit

---
+++ Assets/Move/DragMove.cs
@@ -0,0 +1,16 @@
+﻿/*
+ *  
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragMove : MonoBehaviour
+{
+    Transform _transform;
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+}
