ce8fe43e29257ef38b01ab137face2b7f19eef7a
Mr.True.China <mr.true.china@gmail.com>
2018-10-25 22:28:31 +0900

    Entrance
    增加敌人和玩家入场的效果

--- Assets/Controller/Game Controller/GameController.cs
+++ Assets/Controller/Game Controller/GameController.cs
@@ -1,14 +1,4 @@
-﻿/*
- *  总控器
- *  
- *  包括关卡开始和结束
- *  
- *  开始
- *      隐藏UI
- *      生成玩家和敌人         需要玩家和敌人的预制，敌人可以通过存入解决，拖拽一个默认的来测试
- *          最好是移动进场
- */
-
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,9 +13,16 @@ public class GameController : MonoBehaviour
     GameObject _playerPrefab;
 
     [SerializeField]
+    Vector3 _playerStartEntrancePosition;
+    [SerializeField]
     Vector3 _playerEntrancePosition;
     [SerializeField]
+    Vector3 _enemyStartEntrancePosition;
+    [SerializeField]
     Vector3 _enemyEntrancePosition;
+
+    [SerializeField]
+    float _entranceTime;
     
 
     List<GameObject> _enemys = new List<GameObject>();
@@ -40,26 +37,61 @@ public class GameController : MonoBehaviour
 
 
 
-    //开始游戏
+    //开始游戏，创建玩家和敌人，入场
     public void StartGame()
     {
-        InstantiatePlayer();
-        InstantiateEnemy();
+        StartCoroutine(DoStartGame());
     }
-    void InstantiatePlayer()
+    IEnumerator DoStartGame()
+    {
+        GameObject player = InstantiatePlayer();
+        GameObject enemy = InstantiateEnemy();
+
+        PlayerController playerController = player.GetComponent<PlayerController>();
+        EnemyController enemyController = enemy.GetComponent<EnemyController>();
+
+        playerController.enabled = false;
+        enemyController.enabled = false;
+
+        StartCoroutine(Entrance(player.transform, _playerStartEntrancePosition, _playerEntrancePosition, _entranceTime));
+        StartCoroutine(Entrance(enemy.transform, _enemyStartEntrancePosition, _enemyEntrancePosition, _entranceTime));
+
+        yield return new WaitForSeconds(_entranceTime);
+
+        playerController.enabled = true;
+        enemyController.enabled = true;
+    }
+    GameObject InstantiatePlayer()
     {
         GameObject player = Instantiate(_playerPrefab, _playerEntrancePosition, Quaternion.identity);
         player.AddComponent<PlayerDieTransmitter>().gameController = this;
+
+        return player;
     }
-    void InstantiateEnemy()
+    GameObject InstantiateEnemy()
     {
         GameObject enemy = Instantiate(_enemyPrefab, _enemyEntrancePosition, Quaternion.identity);
         enemy.AddComponent<EnemyDieTransmitter>().gameController = this;
         _enemys.Add(enemy);
+
+        return enemy;
+    }
+    IEnumerator Entrance(Transform transform, Vector3 startPosition, Vector3 endPosition, float entranceTime)
+    {
+        float elapsedTime = 0;
+        while (elapsedTime < entranceTime)
+        {
+            float positionRate = Mathf.InverseLerp(0, entranceTime, elapsedTime);
+            transform.position = Vector3.Lerp(startPosition, endPosition, positionRate);
+            yield return null;
+            elapsedTime += Time.deltaTime;
+        }
+        transform.position = endPosition;
     }
 
 
 
+    //敌人被击破
     public void EnemyDie(GameObject enemy)
     {
         _enemys.Remove(enemy);
@@ -67,15 +99,14 @@ public class GameController : MonoBehaviour
             PlayerWin();
         Debug.Log("Enemy Die");
     }
-
     void PlayerWin()
     {
         Debug.Log("WIN");
     }
 
 
-
-
+    
+    //玩家被击落
     public void PlayerDie()
     {
         Debug.Log("Player Die");
