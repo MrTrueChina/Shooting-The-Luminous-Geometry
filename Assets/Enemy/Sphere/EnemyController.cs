d96d6633be75fdbc14d004598e36f8f263aba958
Mr.True.China <mr.true.china@gmail.com>
2018-10-25 10:15:33 +0900

    Adjustment folder
    调整文件夹和文件层级，集中资源

--- Assets/Enemy/Sphere/EnemyController.cs
+++ Assets/Enemy/Sphere/EnemyController.cs
@@ -3,54 +3,65 @@
 [RequireComponent(typeof(QuadtreeCollider))]
 [RequireComponent(typeof(Life))]
 [RequireComponent(typeof(EnemyShot))]
+[RequireComponent(typeof(EnemyMove))]
 public class EnemyController : MonoBehaviour
 {
     QuadtreeCollider _collider;
     Life _life;
+    EnemyShot _shot;
+    EnemyMove _move;
 
     QuadtreeCollisionEventDelegate _collisionEventDelegate;
     DieEventDelegate _dieEventDelagate;
 
 
 
+    //初始化
     private void Awake()
     {
         GetAndSetCompoents();
         CreatEventDelegate();
-        SubscribeEvents();
     }
-
     void GetAndSetCompoents()
     {
         _collider = GetComponent<QuadtreeCollider>();
         _life = GetComponent<Life>();
+        _shot = GetComponent<EnemyShot>();
+        _move = GetComponent<EnemyMove>();
     }
-
     void CreatEventDelegate()
     {
         _collisionEventDelegate = new QuadtreeCollisionEventDelegate(OnCollision);
         _dieEventDelagate = new DieEventDelegate(OnDie);
     }
 
-    private void SubscribeEvents()
+
+
+    //激活
+    private void OnEnable()
     {
-        SubscribeCollisionEvent();
-        SubscribeDieEvent();
+        SubscribeEvents();
+        EnableCmoponents();
+    }
+    void EnableCmoponents()
+    {
+        _collider.enabled = true;
+        _shot.enabled = true;
+        _move.enabled = true;
     }
 
 
-
+    
+    //碰撞
     void OnCollision(GameObject collider)
     {
         if (collider.tag == "Player Bullet")
             GetShot(collider.GetComponent<Damage>());
     }
-
     void GetShot(Damage damage)
     {
         damage.CauseDamage(_life);
     }
-
     void OnDie()
     {
         Destroy(gameObject);
@@ -58,15 +69,33 @@ public class EnemyController : MonoBehaviour
 
 
 
+    //无效化
     private void OnDisable()
     {
-        UnsubscribeCollisionEvent();
-        UnsubscribeDieEvent();
+        UnsubscribsEvent();
+        DisableCoponents();
+    }
+    void DisableCoponents()
+    {
+        _collider.enabled = false;
+        _shot.enabled = false;
+        _move.enabled = false;
     }
 
 
 
     //订阅和取消订阅
+    void SubscribeEvents()
+    {
+        SubscribeCollisionEvent();
+        SubscribeDieEvent();
+    }
+    void UnsubscribsEvent()
+    {
+        UnsubscribeCollisionEvent();
+        UnsubscribeDieEvent();
+    }
+
     void SubscribeCollisionEvent()
     {
         _collider.collisionEvent += _collisionEventDelegate;
---
+++ Assets/MainController.cs
@@ -0,0 +1,30 @@
+﻿/*
+ *  总控器
+ *  
+ *  包括关卡开始和结束
+ *  
+ *  开始
+ *      隐藏UI
+ *      生成玩家和敌人         需要玩家和敌人的预制，敌人可以通过存入解决，拖拽一个默认的来测试
+ *          最好是移动进场
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject _enemyPrefab;
+    [SerializeField]
+    GameObject _playerPrefab;
+
+
+
+    //选关（存入敌人预制）
+    public void SetEnemyPrefab(GameObject enemyPrefab)
+    {
+        _enemyPrefab = enemyPrefab;
+    }
+}
